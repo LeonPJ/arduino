1c715e46cf96025209e4e79425d2896a90e24b8f
Leon <leon19941214@gmail.com>
2017-09-08 16:08:20 +0800

    DHT-22 n LIBRARY

--- DHT-22/DHT-22.ino
+++ DHT-22/DHT-22.ino
@@ -1,24 +1,102 @@
-#include <dht.h>   
-  
-#define dht_dpin 2 //定義訊號要從Pin A0 進來  
-  
-dht DHT;   
-  
-void setup(){   
-Serial.begin(9600);   
-delay(300);             //Let system settle   
-Serial.println("Humidity and temperature\n\n");   
-delay(700);             //Wait rest of 1000ms recommended delay before   
-                        //accessing sensor   
+#include <dht.h>
+
+dht DHT;
+
+#define DHT22_PIN 9//設定DATA角為D9
+
+struct
+{
+    uint32_t total;
+    uint32_t ok;
+    uint32_t crc_error;
+    uint32_t time_out;
+    uint32_t connect;
+    uint32_t ack_l;
+    uint32_t ack_h;
+    uint32_t unknown;
+} stat = { 0,0,0,0,0,0,0,0};
+
+void setup()
+{
+    Serial.begin(9600);
+    Serial.println("dht22_test.ino");
+    Serial.print("LIBRARY VERSION: ");
+    Serial.println(DHT_LIB_VERSION);
+    Serial.println();
+    Serial.println("Type,\tstatus,\tHumidity (%),\tTemperature (C)\tTime (us)");
 }
-  
-void loop(){   
-DHT.read11(dht_dpin);   //去library裡面找DHT.read11  
-Serial.print("Humidity = ");   
-Serial.print(DHT.humidity);   
-Serial.print("% ");   
-Serial.print("temperature = ");   
-Serial.print(DHT.temperature);   
-Serial.println("C ");   
-delay(1000);            //每1000ms更新一次   
-}  
+
+void loop()
+{
+    // READ DATA
+    Serial.print("DHT22, \t");
+
+    uint32_t start = micros();
+    int chk = DHT.read22(DHT22_PIN);
+    uint32_t stop = micros();
+
+    stat.total++;
+    switch (chk)
+    {
+    case DHTLIB_OK:
+        stat.ok++;
+        Serial.print("OK,\t");
+        break;
+    case DHTLIB_ERROR_CHECKSUM:
+        stat.crc_error++;
+        Serial.print("Checksum error,\t");
+        break;
+    case DHTLIB_ERROR_TIMEOUT:
+        stat.time_out++;
+        Serial.print("Time out error,\t");
+        break;
+    case DHTLIB_ERROR_CONNECT:
+        stat.connect++;
+        Serial.print("Connect error,\t");
+        break;
+    case DHTLIB_ERROR_ACK_L:
+        stat.ack_l++;
+        Serial.print("Ack Low error,\t");
+        break;
+    case DHTLIB_ERROR_ACK_H:
+        stat.ack_h++;
+        Serial.print("Ack High error,\t");
+        break;
+    default:
+        stat.unknown++;
+        Serial.print("Unknown error,\t");
+        break;
+    }
+    // DISPLAY DATA
+    Serial.print(DHT.humidity, 1);
+    Serial.print(",\t");
+    Serial.print(DHT.temperature, 1);
+    Serial.print(",\t");
+    Serial.print(stop - start);
+    Serial.println();
+
+    if (stat.total % 20 == 0)
+    {
+        Serial.println("\nTOT\tOK\tCRC\tTO\tUNK");
+        Serial.print(stat.total);
+        Serial.print("\t");
+        Serial.print(stat.ok);
+        Serial.print("\t");
+        Serial.print(stat.crc_error);
+        Serial.print("\t");
+        Serial.print(stat.time_out);
+        Serial.print("\t");
+        Serial.print(stat.connect);
+        Serial.print("\t");
+        Serial.print(stat.ack_l);
+        Serial.print("\t");
+        Serial.print(stat.ack_h);
+        Serial.print("\t");
+        Serial.print(stat.unknown);
+        Serial.println("\n");
+    }
+    delay(2000);
+}
+//
+// END OF FILE
+//
