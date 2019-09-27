5ad0e41f00ea9dbe751cd1bf0e05b9424299c058
LeonPJ <leon19941214@gmail.com>
2019-09-27 21:26:25 +0800

    WiFi IP n MAC change to string

--- NodeMCUSigeIn/NodeMCUSigeIn.ino
+++ NodeMCUSigeIn/NodeMCUSigeIn.ino
@@ -3,17 +3,14 @@
 
 const char* ssid = "dlink-7730";    // Enter SSID here
 const char* password = "135791113";  //Enter Password here
-
+String IP, MAC;
 void setup() {
   Serial.begin(9600);
   delay(1000);
-
   Serial.println("Connecting to ");
   Serial.println(ssid);
-
   //connect to your local wi-fi network
   WiFi.begin(ssid, password);
-
   //check wi-fi is connected to wi-fi network
   while (WiFi.status() != WL_CONNECTED) {
   delay(500);
@@ -21,32 +18,29 @@ void setup() {
   }
   //Serial.println("");
   Serial.println("WiFi connected");
-  Serial.println(WiFi.localIP());
-  Serial.println(WiFi.macAddress());
+  //Serial.println(WiFi.localIP());
+  //Serial.println(WiFi.macAddress());
+  IP = WiFi.localIP().toString();// change to string
+  MAC = WiFi.macAddress();
 }
  
 void loop() {
+  Serial.println(IP);
+  Serial.println(MAC);
 
  if(WiFi.status() == WL_CONNECTED){   //Check WiFi connection status
-   
    HTTPClient http;    //Declare object of class HTTPClient
-
    http.begin("http://120.126.8.126/api/device?api_token=kB9btlaYg4p7Xokqqbk8YyEna4VehQKwcI7DXwToU6nEjJvW5paJ5ZXZCt22");      //Specify request destination
    http.addHeader("Content-Type", "application/x-www-form-urlencoded");  //Specify content-type header
    int httpCode = http.POST("name=EnvironmentalMonitoring&mac_address=BC%3ADD%3AC2%3A17%3A64%3A58%0A&ip_address=192.168.0.104%0A");   //Send the request
    String payload = http.getString();                  //Get the response payload
- 
    Serial.println(httpCode);   //Print HTTP return code
    Serial.println(payload);    //Print request response payload
- 
    http.end();  //Close connection
  
  }else{
- 
     Serial.println("Error in WiFi connection");   
- 
  }
  
   delay(30000);  //Send a request every 30 seconds
-
 }
